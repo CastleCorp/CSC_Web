7046c5194843f87f18c251be9029086052ba873a
Parker Thomas <pthomas7@duke303-25.elon.edu>
2017-09-25 12:39:28 -0400

    Class September 25

---
+++ Class Exercises/MPG_September25.js
@@ -0,0 +1,5 @@
+alert("The Miles Per Gallon Calculation")
+var miles = prompt("Enter miles driven:")
+var gallons = prompt("Enter gallons of gas used:")
+var mpg = miles/gallons
+alert("You are getting " + mpg + " miles per gallon!")
